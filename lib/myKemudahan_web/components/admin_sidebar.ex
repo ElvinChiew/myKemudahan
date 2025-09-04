60d6e6bc1bf7bef5f7ed4e979813c4d61e781570
ElvinChiew <elvin_chiew@hotmail.com>
2025-09-04 20:46:48 +0800

    add left margin between form the sidemost screen

--- lib/myKemudahan_web/components/admin_sidebar.ex
+++ lib/myKemudahan_web/components/admin_sidebar.ex
@@ -18,7 +18,7 @@ defmodule MyKemudahanWeb.AdminSidebar do
         <div>
           <h2 class="px-3 mb-2 text-sm font-semibold uppercase tracking-wide text-slate-400 sidebar-heading">Overview</h2>
           <a href="/request_list"
-            class="flex items-center space-x-3 px-3 py-2 rounded-md hover:bg-slate-600 transition bg-slate-800"
+            class="flex items-center space-x-3 px-3 py-2 rounded-md hover:bg-slate-600"
             aria-current="page"
             title="Dashboard Overview">
             <i class="fa fa-bar-chart" aria-hidden="true"></i>
@@ -110,7 +110,6 @@ defmodule MyKemudahanWeb.AdminSidebar do
           toggleCollapse('asset-group');
         });
     </script>
-
     """
   end
 end
